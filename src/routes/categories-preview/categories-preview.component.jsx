a39921b741659601e0e4e3b3d69e22db90990d9a
Jonathan Jacob <stevenjacob906@gmail.com>
2023-12-08 13:50:50 +0300

    adding spinner to listen to isLoading in redux

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,13 +1,16 @@
 import { useSelector } from 'react-redux';
 import CategoryPreview from '../../components/category-preview/category-preview.component';
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import Spinner from '../../components/spinner/spinner.component';
+import { selectCategoriesIsLoading, selectCategoriesMap } from '../../store/categories/category.selector';
 
 const CategoriesPreview = () => {
     const categoriesMap = useSelector(selectCategoriesMap);
+    const isLoading = useSelector(selectCategoriesIsLoading);
 
     return(
         <>
             {
+                isLoading ? <Spinner></Spinner> : 
                 Object.keys(categoriesMap).map((title) => {
                     const products = categoriesMap[title];
                     return (
--- src/store/store.js
+++ src/store/store.js
@@ -4,18 +4,24 @@ import { rootReducer } from './root-reducer';
 
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import { thunk } from 'redux-thunk';
+
 
 const persistConfig = {
     key: 'root',
     storage: storage,
+    whitelist: ['cart'],
     blacklist: ['user']
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean);
+const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+// to use redux devtools
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
 
